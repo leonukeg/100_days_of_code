9c4333bf236d9d2ac36de01b6088c8502c3c003d
Freddy Leon <95287339+leonukeg@users.noreply.github.com>
2025-01-23 08:51:56 +0100

    Add files via upload

---
+++ Dia_01/task.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("¿de que ciudad vienes?\n")
+
+pet = input("¿cual fue el nombre de tu primera mascota\n")
+
+print("El nombre de tu banda sera: " + city + " " + pet)
